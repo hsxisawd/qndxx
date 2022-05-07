ae64f020e67c16ddd454732930166f5d6d1f39e6
hsxisawd <83282502+hsxisawd@users.noreply.github.com>
2022-05-08 01:59:50 +0800

    1.2

--- qinniandaxuexi/settings.py
+++ qinniandaxuexi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 's3v$7x&#q2*uas7esvg3w*#t01z4wmfet@1%$mlw+mderqjewl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =False
+DEBUG =True
 
 ALLOWED_HOSTS = ['*',]
 
